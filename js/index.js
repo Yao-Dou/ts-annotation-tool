4c4c0e24f30e614e75f5c916e23195ee7cb36799
Yao Dou <douy@cs.washington.edu>
2022-09-24 20:39:42 -0400

    check more human simplifications

--- js/index.js
+++ js/index.js
@@ -652,7 +652,7 @@ const app = Vue.createApp({
         }
     },
     created: function () {
-        fetch("https://raw.githubusercontent.com/Yao-Dou/ts-annotation-tool/main/data/human_references_58.json")
+        fetch("https://raw.githubusercontent.com/Yao-Dou/ts-annotation-tool/main/data/human_references_a_lot.json")
             .then(r => r.json())
             .then(json => {
                 this.hits_data = json;
